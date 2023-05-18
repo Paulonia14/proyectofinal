65b5ec7f0f8fe529a274405520aeedebea965470
EnzoJP <enzoopalau@gmail.com>
2023-05-18 18:08:32 -0300

    Subo version inicial de load fix element

--- code/uber.py
+++ code/uber.py
@@ -30,15 +30,48 @@ def create_map():
 Largs=sys.argv
 
 try:
+    with open("FE.pickle","rb") as FE_file:
+       F_elements=pk.load(FE_file)
+except:
+    F_elements={}
+
+
+
+try:
     if Largs[1]=="-create_map": #create the map using matrix representation
         Map=create_map()
-        printMat(Map)
+        #printMat(Map)
         #Serialize the map matrix and store it in a file using pickle
         with open("serialized_matrix.pickle","wb") as Matfile:
             pk.dump(Map,Matfile)
         print("map created successfully")
     elif Largs[1]=="-load_fix_element":
-        print("Work in progress")
+        with open("FE.pickle","wb") as FE_file:
+            #verify alredy exits
+            if Largs[2] in F_elements:
+                print("El elemento ya se encuentra cargado")
+                print("desea cambiarlo? 1-Si 0-No")
+                Desc=input()
+                if Desc=="1":
+                    F_elements[Largs[2]]==Largs[2]
+                elif Desc=="0":
+                    print("oki doki")
+                else:
+                    print("Not valid")
+                    raise # error
+            else:
+                #convert to a list-tuple
+                aux=Largs[3]
+                aux="["+ aux + "]"
+                aux=aux.replace('e','')
+                aux=eval(aux)
+                #append to diccionary
+                F_elements[Largs[2]] = aux
+                pk.dump(F_elements,FE_file)#save the structure
+                print("element created successfully")
+                print(F_elements)
+
+
     elif Largs[1]=="-load_movil_element":
         print("Work in progress")
     elif Largs[1]=="-create_trip":
